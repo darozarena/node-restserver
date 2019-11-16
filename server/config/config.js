8576c90117be9ee6e2afce06dd4d5495e3c38f02
David <daudeveloper@gmail.com>
2019-11-16 18:41:04 +0100

    Varible de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -9,9 +9,9 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || "dev";
 
 // ===================
-// ======ENTORNO=======
+// ===BASE de DATOS===
 // ===================
 process.env.URLDB =
     process.env.NODE_ENV === "dev" ?
     "mongodb://localhost:27017/cafe" :
-    "mongodb+srv://strider:Ew7v3GLpy26QXYnY@cluster0-pjlf4.mongodb.net/cafe";
+    process.env.MONGO_URI;
